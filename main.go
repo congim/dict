e4685b432f623262a5ae6d0734d0c843991babf5
shaocongcong <shao.congcong@yalla.live>
2022-07-27 10:02:51 +0800

    正则

--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/congim/dict/dict"
+	"regexp"
 )
 
+func sss() {
+	// test := "a,b,c,d,e"
+	aaa := "我是测试代码AAA"
+	keyWords := []string{"AAA", "BBB", "ccc"}
+	for _, v := range keyWords {
+		reg := regexp.MustCompile("(?i)" + v)
+		fmt.Println(reg)
+		aaa = reg.ReplaceAllString(aaa, "*")
+		fmt.Println(aaa)
+	}
+}
 func main() {
-	dic := dict.New()
-	dic.AddWorld("DDDD", "fuck")
-	fmt.Println(dic.Replace("ddddddddd Fuck", '*'))
+	// dic := dict.New()
+	// dic.AddWorld("DDDD", "fuck")
+	// fmt.Println(dic.Replace("ddddddddd Fuck", '*'))
+	sss()
 }
